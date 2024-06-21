9c180b33cba513c352ce190ae948580e7ea62f7b
Amr7atem <amrhatemanwar@gmail.com>
2024-06-21 07:36:27 +0300

    feat: implemented login and logout

---
+++ src/contexts/FakeAuthContext.jsx
@@ -0,0 +1,66 @@
+import { createContext, useContext, useReducer } from "react";
+
+const AuthContext = createContext();
+
+const initialState = {
+  user: null,
+  isAuthenticated: false,
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "login":
+      return { ...state, user: action.payload, isAuthenticated: true };
+
+    case "logout":
+      return { ...state, user: null, isAuthenticated: false };
+
+    default:
+      throw new Error("Unknown Action");
+  }
+}
+
+const FAKE_USER = {
+  name: "Amr",
+  email: "amr@example.com",
+  password: "qwerty",
+  avatar: "../../public/me.jpg",
+};
+
+function AuthProvider({ children }) {
+  const [{ user, isAuthenticated }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
+
+  function login(email, password) {
+    if (email === FAKE_USER.email && password === FAKE_USER.password) {
+      dispatch({ type: "login", payload: FAKE_USER });
+      return { success: true };
+    } else if (!email || !password) {
+      return { success: false, error: "Please Enter Both email and password" };
+    } else {
+      return { success: false, error: "Invalid email or password" };
+    }
+  }
+
+  function logout() {
+    dispatch({ type: "logout" });
+  }
+
+  return (
+    <AuthContext.Provider value={{ user, isAuthenticated, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+function useAuth() {
+  const context = useContext(AuthContext);
+  if (context === undefined) {
+    throw new Error("AuthContext was used outside AuthProvider");
+  }
+  return context;
+}
+
+export { AuthProvider, useAuth };
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,40 +1,66 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styles from "./Login.module.css";
 import PageNav from "../components/PageNav";
+import { useAuth } from "../contexts/FakeAuthContext";
+import { useNavigate } from "react-router-dom";
+import Button from "../components/Button";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { toastErrOptions } from "../../data/toastOptions";
 
 export default function Login() {
   // PRE-FILL FOR DEV PURPOSES
-  const [email, setEmail] = useState("jack@example.com");
+  const [email, setEmail] = useState("amr@example.com");
   const [password, setPassword] = useState("qwerty");
+  const { login, isAuthenticated } = useAuth();
+  const navigate = useNavigate();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    const { success, error } = login(email, password);
+    if (!success) {
+      toast.error(error, toastErrOptions);
+    }
+  }
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      navigate("/app", { replace: true });
+    }
+  }, [isAuthenticated, navigate]);
 
   return (
-    <main className={styles.login}>
-      <PageNav />
-      <form className={styles.form}>
-        <div className={styles.row}>
-          <label htmlFor="email">Email address</label>
-          <input
-            type="email"
-            id="email"
-            onChange={(e) => setEmail(e.target.value)}
-            value={email}
-          />
-        </div>
+    <>
+      <main className={styles.login}>
+        <PageNav />
+        <form className={styles.form} onSubmit={handleSubmit}>
+          <div className={styles.row}>
+            <label htmlFor="email">Email address</label>
+            <input
+              type="email"
+              id="email"
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
+            />
+          </div>
 
-        <div className={styles.row}>
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            id="password"
-            onChange={(e) => setPassword(e.target.value)}
-            value={password}
-          />
-        </div>
+          <div className={styles.row}>
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              onChange={(e) => setPassword(e.target.value)}
+              value={password}
+            />
+          </div>
 
-        <div>
-          <button>Login</button>
-        </div>
-      </form>
-    </main>
+          <div>
+            <Button type="primary">Login</Button>
+          </div>
+        </form>
+      </main>
+      <ToastContainer />
+    </>
   );
 }
